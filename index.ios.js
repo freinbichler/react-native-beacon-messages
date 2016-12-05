0df3ed424b1f9f4d68a40b0c3fd69c9e22c4448b
Lisi Linhart <liisilein@hotmail.com>
2016-12-05 13:46:02 +0100

    app structure added

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,7 @@
  */
 
 import React, { Component } from 'react';
+import BeaconMessages from './app';
 import {
   AppRegistry,
   StyleSheet,
@@ -12,49 +13,11 @@ import {
   View
 } from 'react-native';
 
-var Beacons = require('react-native-ibeacon');
+//var Beacons = require('react-native-ibeacon');
 
 var region = {
   identifier: 'Estimote Candy',
   uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D:29048:61507'
 };
-
-export default class BeaconMessages extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Look for messages!
-        </Text>
-        {/*<Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>*/}
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('BeaconMessages', () => BeaconMessages);
